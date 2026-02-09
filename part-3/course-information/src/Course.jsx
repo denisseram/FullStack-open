15e8c615e309e932f0045a23db67c692b2b20e0e
denisseram <a01562077@tec.mx>
2026-02-09 14:19:40 +0100

    2.1: Course information step 6

---
+++ part-3/course-information/src/Course.jsx
@@ -0,0 +1,32 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
