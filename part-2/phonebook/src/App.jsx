9f95ba561eb5ac085537dae3a26e62306e954201
denisseram <a01562077@tec.mx>
2026-02-11 07:35:17 +0100

    2.7: The Phonebook Step 2

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -8,6 +8,14 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
+    
+    const nameExists = persons.some(person => person.name.toLowerCase() === newName.toLowerCase())
+    
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    
     const newPerson = { name: newName }
     setPersons(persons.concat(newPerson))
     setNewName('')
